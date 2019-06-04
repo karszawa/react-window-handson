61b90218b6e1610677768d325b54ac5611b07fc6
Hiroaki KARASAWA <karszawa@mercari.com>
2019-06-04 18:15:54 +0900

    Use InfiniteLoader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { FixedSizeList } from "react-window";
+import InfiniteLoader from "react-window-infinite-loader";
 
 const RowContainer = styled.div`
   position: relative;
@@ -22,36 +23,84 @@ const RowImage = styled.img`
   height: 100%;
 `;
 
-const Row = ({ index, style }) => (
-  <RowContainer style={style}>
-    <RowName>{items[index].name}</RowName>
-    <RowImage src={items[index].url} />
-  </RowContainer>
-);
+const List = ({ items, moreItemsLoading, hasNextPage, loadMoreItems }) => {
+  const Row = ({ index, style }) => {
+    const itemLoading = index >= items.length;
 
-const items = new Array(100).fill(0).map((_, j) => ({
-  name: `photo-${j}`,
-  url: `https://picsum.photos/id/${j}/200/300`
-}));
+    if (itemLoading) {
+      return <RowContainer style={{ ...style, backgroundColor: "grey" }} />;
+    } else {
+      return (
+        <RowContainer style={style}>
+          <RowName>{items[index].name}</RowName>
+          <RowImage src={items[index].url} />
+        </RowContainer>
+      );
+    }
+  };
 
-export default function App() {
+  const itemCount = hasNextPage ? items.length + 1 : items.length;
+
+  console.log(items.length);
+
   return (
-    <div className="App">
-      <FixedSizeList
-        height={1000}
-        width={100}
-        itemSize={120}
-        itemCount={items.length}
-      >
-        {Row}
-      </FixedSizeList>
-    </div>
+    <InfiniteLoader
+      isItemLoaded={index => index < items.length - 1}
+      itemCount={itemCount}
+      loadMoreItems={loadMoreItems}
+    >
+      {({ onItemsRendered, ref }) => (
+        <FixedSizeList
+          height={1000}
+          width={100}
+          itemCount={itemCount}
+          itemSize={120}
+          onItemsRendered={onItemsRendered}
+          ref={ref}
+        >
+          {Row}
+        </FixedSizeList>
+      )}
+    </InfiniteLoader>
   );
+};
+
+const ENDPOINT = "https://api.thecatapi.com/v1/images/search";
+const LIMIT = 10;
+
+export default class App extends React.Component {
+  state = {
+    items: [],
+    moreItemsLoading: false,
+    page: 0
+  };
+
+  async loadMoreItems(startIndex, stopIndex) {
+    console.log("Loading...");
+
+    this.setState({ moreItemsLoading: true });
+
+    const response = await fetch(
+      `${ENDPOINT}?limit=${LIMIT}&page=${startIndex / LIMIT}`
+    );
+    const data = await response.json();
+    const items = data.map(e => ({ name: `cat-${e.id}`, url: e.url }));
+
+    this.setState({
+      moreItemsLoading: false,
+      page: this.state.page + 1,
+      items: this.state.items.concat(items)
+    });
+  }
+
+  render() {
+    return (
+      <List
+        items={this.state.items}
+        moreItemsLoading={this.state.moreItemsLoading}
+        hasNextPage={true}
+        loadMoreItems={this.loadMoreItems.bind(this)}
+      />
+    );
+  }
 }
-
-/*
-
-* Create
-*
-
-*/
